b4fe43d97b0e46418eba7ee9739243c93588ae8d
Yousef El-khawanki <104323076+Y19M97@users.noreply.github.com>
2023-01-08 01:45:07 +0200

    edit

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ screen.addEventListener('click',function(){
 // ----------------------
 $(document).ready(
     ()=>$('#isLoading i').fadeOut(4000,()=>$('#isLoading').fadeOut(1000,()=>$('#about .col-md-6').animate({'display':'block' , 'opacity':'1' },2000 ,function(){
-        $('#screen').animate({'opacity':'1', 'top':'50%'},2000 ,()=>{
+        $('#screen').animate({'opacity':'1'},2000 ,()=>{
             $('#isLoading').remove()
             $('body').css('overflow', 'auto')
         }
